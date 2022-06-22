f849d63e354cbbbda4e2d16ce8c5df503dc89b67
jaehyeon <morphine.coder@gmail.com>
2022-06-22 14:32:18 +0900

    Initial commit

---
+++ app/src/main/java/com/jaehyeon/compose/calccompose/CalculatorButton.kt
@@ -0,0 +1,14 @@
+package com.jaehyeon.compose.calccompose
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+
+@Composable
+fun CalculatorButton(
+    symbol: String,
+    modifier: Modifier,
+    onClick: () -> Unit
+) {
+
+}
