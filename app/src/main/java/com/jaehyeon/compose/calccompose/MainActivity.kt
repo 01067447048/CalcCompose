c19eb63cbee32773b78fb4d940a7da3b83e1deb6
jaehyeon <morphine.coder@gmail.com>
2022-06-23 17:21:54 +0900

    finish UI

--- app/src/main/java/com/jaehyeon/compose/calccompose/MainActivity.kt
+++ app/src/main/java/com/jaehyeon/compose/calccompose/MainActivity.kt
@@ -3,41 +3,41 @@ package com.jaehyeon.compose.calccompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.jaehyeon.compose.calccompose.ui.theme.CalcComposeTheme
+import com.jaehyeon.compose.calccompose.ui.theme.MediumGray
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             CalcComposeTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
+                val viewModel = viewModel<CalculatorViewModel>()
+                val state = viewModel.state
+                val buttonSpacing = 8.dp
+
+                Calculator(
+                    state = state,
+                    onAction = viewModel::onAction,
+                    buttonSpace = buttonSpacing,
+                    modifier = Modifier.fillMaxSize()
+                        .background(MediumGray)
+                        .padding(16.dp)
+                )
+
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    CalcComposeTheme {
-        Greeting("Android")
     }
 }
